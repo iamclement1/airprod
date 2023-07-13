0cf6bf9825587a854c156c6b9f7418ce44adffa3
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-07-13 21:25:32 +0100

    feat: all projects data :plane:

---
+++ src/utils/data.js
@@ -0,0 +1,22 @@
+export const servicesData = [
+   {
+      id: 1,
+      img: "/images/code.svg",
+      desc: "Software Development & Devsecops"
+   },
+   {
+      id: 2,
+      img: "/images/talent.svg",
+      desc: "Hire a Tech Talent"
+   },
+   {
+      id: 3,
+      img: "/images/design.svg",
+      desc: "Product Design & Branding"
+   },
+   {
+      id: 4,
+      img: "/images/deployment.svg",
+      desc: "Application Deployment"
+   }
+]

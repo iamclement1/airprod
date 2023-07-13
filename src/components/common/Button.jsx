cc2abd81640d01799858111d6df9288703c31963
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-07-13 19:48:10 +0100

    update: fixed padding, margin and rounded curve :bug:

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -7,7 +7,7 @@ const Button = ({ text,
    return (
       <button {...props} 
       onClick={handleButton}
-         className='bg-[#E91313] text-white rounded-md p-4'>
+         className='bg-[#E91313] text-white rounded-xl px-4 py-3'>
             {text}
          </button>
    )

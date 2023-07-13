9e8e1259ad746d4c51388acce7bc58460c085bb4
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-07-13 21:22:10 +0100

    'feat: all components exported in a file :tada:

---
+++ src/components/my-pages/index.js
@@ -0,0 +1,6 @@
+'use client'
+export { default as About } from './About';
+// export { default as Contact } from './Contact'
+export { default as Newsletter } from './Newsletter';
+export { default as Services } from './Services';
+export { default as Solutions } from './Solutions';

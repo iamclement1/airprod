58f2d99f4f078bac7a185998571c8dcc1df09f79
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-07-13 19:47:50 +0100

    update: added application favicon :tada:

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel='icon' href='/images/logo.svg' />
+      </head>
       <body className={noto.className}>{children}</body>
     </html>
   )

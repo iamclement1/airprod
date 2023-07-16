8a13bc373ca183b0841545f4977c19e109cc268b
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-07-16 21:43:32 +0100

    update: worked on responsiveness for navbar and hero section :tada:

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -10,14 +10,16 @@ const Header = () => {
       setIsMobileMenuOpen(!isMobileMenuOpen);
    };
    return (
-      <nav className='lg:bg-black/95 sm:bg-white transition-all ease-in-out' >
+      <nav className='lg:bg-[#0A0B0B] md:bg-[#0A0B0B] bg-white/50 transition-all ease-in-out relative z-50' >
          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-12 lg:py-2">
             <div className="flex items-center justify-between h-16">
                <div className="flex items-center">
                   <div className="flex-shrink-0">
                      <Link href="/">
                         <Image src={"/images/logo.svg"} alt="brand-logo" width={100}
-                           height={200} />
+                           height={200} className='lg:block md:block hidden'/>
+                        <Image src={"/images/logo-mobile.svg"} alt="brand-logo" width={100}
+                           height={200} className='lg:hidden md:hidden block' />
                      </Link>
                   </div>
                </div>
@@ -47,16 +49,26 @@ const Header = () => {
 
                {/* Hamburger menu for mobile view */}
                <div className="flex md:hidden">
-                  <button
-                     onClick={toggleMobileMenu}
-                  // className="text-gray-400 hover:text-white focus:outline-none focus:text-white"
-                  >
-                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
-                        <path d="M27.1249 14.364H4.87517C4.00264 14.364 3.23914 15.1276 3.23914 16C3.23914 16.8725 4.0027 17.6361 4.87517 17.6361H27.1249C27.9974 17.6361 28.7609 16.8725 28.7609 16C28.7609 15.1276 27.9973 14.364 27.1249 14.364Z" fill="#E91313" />
-                        <path d="M27.1249 23.8528H4.87517C4.00264 23.8528 3.23914 24.6163 3.23914 25.4888C3.23914 26.3613 4.0027 27.1248 4.87517 27.1248H27.1249C27.9974 27.1248 28.7609 26.3613 28.7609 25.4888C28.7609 24.6163 27.9973 23.8528 27.1249 23.8528Z" fill="#E91313" />
-                        <path d="M4.87517 8.14706H27.1249C27.9974 8.14706 28.7609 7.3835 28.7609 6.51103C28.7609 5.63856 27.9973 4.875 27.1249 4.875H4.87517C4.00264 4.875 3.23914 5.63856 3.23914 6.51103C3.23914 7.3835 4.0027 8.14706 4.87517 8.14706Z" fill="#E91313" />
-                     </svg>
-                  </button>
+                  {
+                     isMobileMenuOpen === false ? (
+                        <button
+                           onClick={toggleMobileMenu}
+                        // className="text-gray-400 hover:text-white focus:outline-none focus:text-white"
+                        >
+                           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+                              <path d="M27.1249 14.364H4.87517C4.00264 14.364 3.23914 15.1276 3.23914 16C3.23914 16.8725 4.0027 17.6361 4.87517 17.6361H27.1249C27.9974 17.6361 28.7609 16.8725 28.7609 16C28.7609 15.1276 27.9973 14.364 27.1249 14.364Z" fill="#E91313" />
+                              <path d="M27.1249 23.8528H4.87517C4.00264 23.8528 3.23914 24.6163 3.23914 25.4888C3.23914 26.3613 4.0027 27.1248 4.87517 27.1248H27.1249C27.9974 27.1248 28.7609 26.3613 28.7609 25.4888C28.7609 24.6163 27.9973 23.8528 27.1249 23.8528Z" fill="#E91313" />
+                              <path d="M4.87517 8.14706H27.1249C27.9974 8.14706 28.7609 7.3835 28.7609 6.51103C28.7609 5.63856 27.9973 4.875 27.1249 4.875H4.87517C4.00264 4.875 3.23914 5.63856 3.23914 6.51103C3.23914 7.3835 4.0027 8.14706 4.87517 8.14706Z" fill="#E91313" />
+                           </svg>
+                        </button>
+                     ) : (
+                        <button onClick={toggleMobileMenu}>
+                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                              <path d="M2 22L12 12L22 22M22 2L11.9981 12L2 2" stroke="#E91313" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
+                           </svg>
+                        </button>
+                     )
+                  }
                </div>
             </div>
          </div>
@@ -76,7 +88,7 @@ const Header = () => {
                <Link href="#services" className='block text-center text-black hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium'>
                   Become A Talent
                </Link>
-               <div className='md:hidden block border-black border'>
+               <div className='md:hidden block'>
                   <Button text="Get Started" />
                </div>
             </div>
--- src/components/common/Hero.jsx
+++ src/components/common/Hero.jsx
@@ -1,36 +1,67 @@
-import Image from 'next/image'
-import React from 'react'
-import Button from './Button'
-import Link from 'next/link'
+
+import Image from "next/image";
+import React from "react";
+import Button from "./Button";
+import Link from "next/link";
 
 const Hero = () => {
    return (
       <main>
          <section>
-            <div className='w-full relative'>
-               <Image src={"/images/hero.svg"} alt='Hero-Image' width={500} height={500}
-                  className='w-full' />
+            <div className={`w-full relative`}>
+               <Image
+                  src={"/images/hero.svg"}
+                  alt="Hero-Image"
+                  width={500}
+                  height={500}
+                  className={`w-full lg:block md:block hidden`}
+               />
+               <Image
+                  src={"/images/hero-mobile.svg"}
+                  alt="Hero-Image"
+                  width={500}
+                  height={500}
+                  className={`w-full lg:hidden md:hidden block `}
+               />
             </div>
-            <div className='absolute lg:top-72 lg:mx-28'>
-               <h1 className='lg:w-6/12 text-white lg:text-4xl font-bold '>
+            <div className="absolute lg:top-72 md:top-32 top-28 lg:mx-28 md:mx-8 mx-6">
+               <h1 className="lg:w-6/1 md:w-6/12 w-10/12 text-white lg:text-4xl md:text-2xl text-2xl font-bold ">
                   Empowering your vision with innovative technology solutions.
                </h1>
-               <p className='text-[#BFBBBB] lg:my-8'>
+               <p className="text-[#BFBBBB] lg:my-8 md:my-5 my-5 md:w-6/12 w-4/6 lg:text-xl md:text-md text-xs">
                   {"We deliver high quality, scalable and user friendly software"}
                </p>
-               <div className='flex items-center space-x-6'>
-                  <Link href={'/contact'}>
-                     <button className='bg-[#E91313] text-white lg:px-7 lg:py-4 p-3 rounded-xl'>
+               <div className="flex items-center space-x-6">
+                  <Link href={"/contact"}>
+                     <button className="bg-[#E91313] text-white lg:px-7 lg:py-4 p-3 rounded-xl">
                         Get Started
                      </button>
                   </Link>
-                  <Link href={'/contact'}>
-                     <button className="flex items-center border-2 border-white p-3 rounded-xl  text-white">
+                  <Link href={"/contact"}>
+                     <button className="flex items-center md:border-2 md:border-white p-3 rounded-xl  md:text-white text-[#BFBBBB]">
                         Learn more
-                        <span className='ml-2'>
-                           <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
-                              <path d="M14.93 18.82C14.74 18.82 14.55 18.75 14.4 18.6C14.11 18.31 14.11 17.83 14.4 17.54L19.94 12L14.4 6.46C14.11 6.17 14.11 5.69 14.4 5.4C14.69 5.11 15.17 5.11 15.46 5.4L21.53 11.47C21.82 11.76 21.82 12.24 21.53 12.53L15.46 18.6C15.31 18.75 15.12 18.82 14.93 18.82Z" fill="white" />
-                              <path d="M20.83 12.75H4C3.59 12.75 3.25 12.41 3.25 12C3.25 11.59 3.59 11.25 4 11.25H20.83C21.24 11.25 21.58 11.59 21.58 12C21.58 12.41 21.24 12.75 20.83 12.75Z" fill="white" />
+                        <span className="ml-2 md:block hidden">
+                           <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="25"
+                              height="24"
+                              viewBox="0 0 25 24"
+                              fill="none"
+                           >
+                              <path
+                                 d="M14.93 18.82C14.74 18.82 14.55 18.75 14.4 18.6C14.11 18.31 14.11 17.83 14.4 17.54L19.94 12L14.4 6.46C14.11 6.17 14.11 5.69 14.4 5.4C14.69 5.11 15.17 5.11 15.46 5.4L21.53 11.47C21.82 11.76 21.82 12.24 21.53 12.53L15.46 18.6C15.31 18.75 15.12 18.82 14.93 18.82Z"
+                                 fill="white"
+                              />
+                              <path
+                                 d="M20.83 12.75H4C3.59 12.75 3.25 12.41 3.25 12C3.25 11.59 3.59 11.25 4 11.25H20.83C21.24 11.25 21.58 11.59 21.58 12C21.58 12.41 21.24 12.75 20.83 12.75Z"
+                                 fill="white"
+                              />
+                           </svg>
+                        </span>
+                        <span className="ml-2 md:hidden block">
+                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                              <path d="M9.62 12.5466C9.49333 12.5466 9.36667 12.5 9.26667 12.4C9.07333 12.2066 9.07333 11.8866 9.26667 11.6933L12.96 7.99996L9.26667 4.30662C9.07333 4.11329 9.07333 3.79329 9.26667 3.59996C9.46 3.40662 9.78 3.40662 9.97333 3.59996L14.02 7.64662C14.2133 7.83996 14.2133 8.15996 14.02 8.35329L9.97333 12.4C9.87333 12.5 9.74667 12.5466 9.62 12.5466Z" fill="#BFBBBB" />
+                              <path d="M13.5533 8.5H2.33334C2.06 8.5 1.83334 8.27333 1.83334 8C1.83334 7.72667 2.06 7.5 2.33334 7.5H13.5533C13.8267 7.5 14.0533 7.72667 14.0533 8C14.0533 8.27333 13.8267 8.5 13.5533 8.5Z" fill="#BFBBBB" />
                            </svg>
                         </span>
                      </button>
@@ -39,7 +70,7 @@ const Hero = () => {
             </div>
          </section>
       </main>
-   )
-}
+   );
+};
 
-export default Hero
+export default Hero;

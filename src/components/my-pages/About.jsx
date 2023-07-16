3b2e7a9521193047cbd3555935a58ea3d5936be4
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-07-16 20:32:38 +0100

    feat: added other components on the landing page  with modifications ina about and services page :tada:

--- src/components/my-pages/About.jsx
+++ src/components/my-pages/About.jsx
@@ -22,14 +22,14 @@ const About = () => {
 
                </div>
 
-               <div className=" rounded-md p-4 bg-[#AEF0FE]/10 border border-[#AEF0FE]">
+               <div className=" rounded-md p-1 bg-[#AEF0FE]/10 border border-[#AEF0FE]">
                   <svg xmlns="http://www.w3.org/2000/svg" width="120" height="88" viewBox="0 0 120 88" fill="none"
                      className="mx-auto my-3">
                      <path d="M60 0L64.239 20.0604L80.5061 9.07969L70.2338 26.4687L89 31L70.2338 35.5313L80.5061 52.9203L64.239 41.9396L60 62L55.761 41.9396L39.4939 52.9203L49.7662 35.5313L31 31L49.7662 26.4687L39.4939 9.07969L55.761 20.0604L60 0Z" fill="#28D1DC" />
                      <path d="M120 67.0319C120 78.3538 93.1371 87.5319 60 87.5319C26.8629 87.5319 0 78.3538 0 67.0319C0 55.7101 26.8629 67.032 60 67.032C93.1371 67.032 120 55.7101 120 67.0319Z" fill="#A0CBDD" />
                   </svg>
-                  <p className="text-[#137B82] text-sm text-center mt-4">
-                     {"We handle all round software development from origin to deployment."}
+                  <p className="w-10/12 mx-auto text-[#137B82] text-sm text-center mt-4">
+                     {"Need to rebuild? ApiPod is your best option. Infusing the skills from talented minds to give you the best product."}
                   </p>
 
                </div>
@@ -41,7 +41,7 @@ const About = () => {
                      <path d="M-4.13537e-06 94.4865C-2.80275e-06 79.2431 29.0092 59 44.5 59C59.9908 59 89 79.2431 89 94.4865C89 109.73 59.9908 81.4748 44.5 81.4748C29.0092 81.4748 -5.46799e-06 109.73 -4.13537e-06 94.4865Z" fill="#B9B9F0" />
                   </svg>
                   <p className="text-[#3D3DEA] text-sm text-center mt-4">
-                     {"We handle all round software development from origin to deployment."}
+                     {"Products that meets the requirements of the owner and connects seamlessly to the end user."}
                   </p>
 
                </div>
